ce008f63db3e9dc15a3f4a878bed82efc9e596e7
Adam Feehely <adamfeehely@gmail.com>
2024-08-19 14:00:09 +0000

    add env.py and update with database url

--- tewix/settings.py
+++ tewix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5h_ritq@79jv@h!67222)n9hp2m^8cylb4!10)7iyqad$__e7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['8000-adamfcode-tewixblog-wb9lxtupekm.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
